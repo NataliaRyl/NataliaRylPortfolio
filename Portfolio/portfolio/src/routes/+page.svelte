59e7bf1d5a3b1ebf518fd081032c10c08bed9fb8
Natalia Ryl <C00294480@setu.ie>
2025-04-19 13:14:58 +0100

    had to edit the homepage

--- Portfolio/portfolio/src/routes/+page.svelte
+++ Portfolio/portfolio/src/routes/+page.svelte
@@ -7,26 +7,30 @@
 </script>
 
 <div class="container">
-    <div class="intro">
-        <h1>Natalia Ryl</h1>
-        <p>Hey, I'm Natalia!<br>
-            I love bringing ideas to life through animation and concept art. 
-            I enjoy making games for fun and I like to design logos on the side.
-            I'm also exploring UI/UX design and how art shapes experiences.
-        </p>
-    </div>
-    
-    <div class="logo">
-        <img src="/logo.png" alt="Designs by Natie Logo">
-    </div>
+	<div class="intro">
+		<h1>Natalia Ryl</h1>
+		<div class="intro-textbox">
+			<p>Hey, I'm Natalia!</p>
+			<p>
+				I love bringing ideas to life through animation and concept art.
+				I enjoy making games for fun and I like to design logos on the side.
+				I'm also exploring UI/UX design and how art shapes experiences.
+			</p>
+		</div>
+	</div>
+
+	<div class="logo">
+		<img src="/logo.png" alt="Designs by Natie Logo">
+	</div>
 </div>
 
 <style>
     .container {
         display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding-top: 0px;
+		align-items: flex-start;
+		justify-content: space-between;
+		padding: 10px 80px;
+		gap: 60px;
     }
     
     .intro {
@@ -35,29 +39,36 @@
     
     .logo {
         flex: 1;
-        display: flex;
-        justify-content: center;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+        padding-top: 40px;
     }
     
     h1 {
         text-align: left;
-        padding-bottom: 20px;
+        margin-bottom: 30px;
         font-size: 48px;
-        font-weight: 500;
+        font-weight: 450;
     }
-    
-    p {
+
+    .intro-textbox {
+		background-color: #a8bba9;
+		box-shadow: 15px 15px 0px #526253;
+		border-radius: 10px;
+		padding: 20px 30px;
+		max-width: 520px;
+		color: #f1f1f1;
+	}
+
+    .intro-textbox p {
+		font-size: 26px;
+		margin: 0 0 10px 0;
         text-align: left;
-        color: white;
-        background-color: #a8bba9;
-        box-shadow: 15px 15px #526253;
-        max-width: 500px;
-        padding: 20px;
-        border-radius: 10px;
-        font-size: 26px;
-    }
+	}
     
     img {
         height: 450px;
+        transform: translateX(60px); /* pushes it right */
     }
 </style>
