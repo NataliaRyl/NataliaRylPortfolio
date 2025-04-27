bd275be5ecd54a2db0880c84b1eb1be96ef46d2c
Natalia Ryl <C00294480@setu.ie>
2025-04-27 21:02:51 +0100

    cv page added

--- Portfolio/portfolio/src/routes/cv/+page.svelte
+++ Portfolio/portfolio/src/routes/cv/+page.svelte
@@ -5,3 +5,74 @@
         console.log("CV Page Loaded");
     });
 </script>
+
+<h1>My CV</h1>
+
+<div class="wrapper">
+    <div class="cv-content">
+        <img src="/CV1.png" alt="CV Page 1" class="cv-page">
+        <img src="/CV2.png" alt="CV Page 2" class="cv-page">
+    </div>
+
+    <a href="/Natalia_Ryl_CV.pdf" download="Natalia_Ryl_CV.pdf" class="download-link">Download CV</a>
+</div>
+
+<style>
+    h1 {
+        font-size: 48px;
+        font-weight: 450;
+        margin-bottom: 30px;
+        text-align: left;
+        padding-left: 207px;
+        padding-top: 10px;
+    }
+    
+    .wrapper {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        margin: 0 auto;
+        width: 80%;
+        max-width: 1200px;
+        box-sizing: border-box;
+    }
+
+    .cv-content {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 20px;
+        max-height: 800px;
+        overflow-y: auto;
+        text-align: left;
+        width: 70%;
+        max-width: 800px;
+    }
+
+    .cv-page {
+        width: 100%; 
+        margin-bottom: 20px; 
+        max-width: 800px;
+        border-radius: 10px;
+        box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .download-link {
+        display: inline-block;
+        margin-top: 200px;
+        font-size: 18px;
+        color: #a8bba9;
+        text-decoration: none;
+        padding: 10px 20px;
+        background-color: #f1f1f1;
+        border-radius: 8px;
+        box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+        transition: all 0.3s ease;
+    }
+
+    .download-link:hover {
+        background-color: #a8bba9;
+        color: white;
+    }
+
+</style>
