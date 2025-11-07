45258ff286c6cc0a097d542a537e19977bedf333
Natalia Ryl <c00294480@setu.ie>
2025-11-07 00:41:37 +0000

    deploy to github pages

---
+++ Portfolio/portfolio/_app/immutable/chunks/DnYTLbTf.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1hcob4t)==null?void 0:s.base)??"/NataliaRylPortfolio";var t;const o=((t=globalThis.__sveltekit_1hcob4t)==null?void 0:t.assets)??a;export{o as a,a as b};
